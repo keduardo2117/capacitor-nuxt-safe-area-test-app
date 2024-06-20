c6b2e88c285c0b3e2a0b9ee27525d4ba87bf264a
keduardo2117 <Eduardo.carrillo@enumma.com>
2024-06-20 12:12:39 -0600

    Test app safe area

---
+++ app.vue
@@ -0,0 +1,26 @@
+<template>
+  <NuxtPage />
+</template>
+<script setup lang="ts">
+import { SafeArea } from "@capacitor-community/safe-area";
+
+SafeArea.enable({
+  config: {
+    customColorsForSystemBars: true,
+    statusBarColor: "#00000000", // transparent
+    statusBarContent: "light",
+    navigationBarColor: "#00000000", // transparent
+    navigationBarContent: "light",
+  },
+});
+//npx
+//npx nuxi@latest init <project-name>
+//pnpm
+//pnpm dlx nuxi@latest init <project-name>
+//npx
+// npx nuxi@latest module add ionic
+// pnpm dlx nuxi@latest module add ionic
+// cap sync
+// npx cap sync
+//  pnpm dlx cap sync
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <div class="bg-white text-black">Welcome</div>
+</template>
+<script setup lang="ts"></script>
